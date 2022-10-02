3590e8b85cfc396c27067c20c29e69d993fcf355
EldiAnugrah <203040031@mail.unpas.ac.id>
2022-10-02 22:33:54 +0700

    menyelesaikan video pendahuluan OOP PHP

---
+++ oop/pendahuluan.php
@@ -0,0 +1,54 @@
+Prosedural programing , contohnya di video belajar php untuk pemula 
+
+karakteristik prosedural programing 
+1.Instruksinya dilakukan selangkah demi selangkah 
+2.Memecah program menjadi bagian kecil 
+3.Biasa disebut prosedur ,subroutine atau function
+4.Pembacaan dilakukan secara linier/top to buttom
+5.Biasanya ada di bahasa pemrograman : fotran,ALGOL,COBOL,Pascal,C,PHP dan Javascript 
+
+Alasan memilih prosedural
+-cenderung to the point 
+-kemudahan untuk dibaca oleh interpreter & compiler 
+-mudah ditelusuri 
+-lebih menggunakan sedikit memori karena to the point 
+
+
+karakteristik OOP
+1.Menyusun semua kode program dan struktur data sebagai objek 
+2.Objek unit dasar pembentuk program 
+3.Dapat menyimpan data dari perilaku program , yang ada properti dan method 
+4.Objek saling berinteraksi 
+5.Biasanya ada di bahasa pemrograman : java,ruby,phyton,c++,javascript,php5
+
+Alasan memilih OOP
+-lebih mudah merepresentrasikan dari dunia nyata 
+-enkapsulasi (membuat data yang dibuat aman karena tidak dapat diakses oleh bagia lain), abstraksi data (mudah menyembunyikan program)
+-reusability yang telah dibuat sebelumnya 
+-skalabilitas dan ekstensibilitas , yang memudahkan programer yang aplikasinya akan menjadi lebih besar karena terpisah
+-mudah mengelola 
+-mudah berkolaborasi dengan yang lainnya 
+-digunakan oleh framework 
+
+mana yang lebih baik ? Jawabannya tidak ada karena masing masing memiliki fokusnya sendiri, jika ingin membuat program sederhana yang dibuat 
+oleh sorang saja  lebih baik menggunakan prosedural, namun jika kebalikannya maka oop lebih baik.
+
+konsep OOP pada php 
+~ Basic
+
+-kelas dan object 
+-properti dan method 
+-konstruktor
+-Objek type 
+-inheritance
+-visibility 
+-setter dan getter 
+-statik method
+
+~advence
+-abstraksi dan interface
+-interception 
+-object cloning 
+-callbacks dan closeures
+-namespaces dan autoloading  
+
