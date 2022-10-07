16fb1a62eb2108c07c7ecbd32af9a7f595efad26
EldiAnugrah <203040031@mail.unpas.ac.id>
2022-10-07 09:07:11 +0700

    menyelesaikan video 6

---
+++ MVC/app/core/Constants.php
@@ -0,0 +1,4 @@
+<?php 
+
+//menyimpan url 
+define('BASEURL','http://localhost/prakweb_A_203040031/MVC/public/');
--- MVC/app/views/about/index.php
+++ MVC/app/views/about/index.php
@@ -1,2 +1,5 @@
-  <h1>About ELDI </h1>
+  <div class="container">
+  <h1 class="mt-4">About ELDI </h1>
+  <img src="<?= BASEURL; ?>img/nopoto.png" alt="mamang" width="200px;" class="rounded-circle shadow">
   <p>Halo ges nama saya <?= $data['nama']; ?> saya adalah  <?= $data['pekerjaan']; ?>  dan berumur <?= $data['umur']; ?></p>
+  </div>
