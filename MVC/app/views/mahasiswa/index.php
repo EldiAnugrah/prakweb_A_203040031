45e6e6b6eff2f99affa07f87c3a81d3553ce26b9
EldiAnugrah <203040031@mail.unpas.ac.id>
2022-10-10 14:23:06 +0700

    menyelesaikan video 9(DLC ASISTEN)

--- MVC/app/views/mahasiswa/index.php
+++ MVC/app/views/mahasiswa/index.php
@@ -1,10 +1,13 @@
-<div class="container mt-5">
+<div class="container mt-3">
 
 <div class="row">
-  <div class="col-">
-    <h3>daftar mahasiswa </h3>
+  <div class="col-6">
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#formModal">
+      Tambah Data Mahasiswa
+    </button>
+
+    <h3 class="mt-3">daftar mahasiswa </h3>
         <ul class="list-group">
-          <h3>Nama</h3>
         <?php foreach($data['mhs'] as $mhs): ?>
           <li class="list-group-item d-flex justify-content-between align-items-center"> 
             <?= $mhs['nama']; ?> 
@@ -14,5 +17,49 @@
         </ul> 
   </div>
 </div>
-
+</div>
+<!-- Modal -->
+<div class="modal fade" id="formModal" tabindex="-1" role="dialog" aria-labelledby="judulModal" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="judulModal">Tambah Data Mahasiswa</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form action="<?= BASEURL; ?>mahasiswa/tambah" method="post">
+          <div class="form-group">
+            <label for="nama">Nama</label>
+            <input type="text" class="form-control" id="nama" name= "nama" placeholder="Eldi Tampan">
+          </div>
+          <div class="form-group">
+            <label for="nrp">nrp</label>
+            <input type="number" class="form-control" id="nrp" name= "nrp" placeholder="203040031">
+          </div>
+          <div class="form-group">
+            <label for="email">email</label>
+            <input type="email" class="form-control" id="email" name= "email" placeholder="elditamvan15@gmail.com">
+          </div>
+        <div class="form-group">
+          <label for="jurusan">Jurusan</label>
+            <select class="form-control" id="jurusan" name="jurusan">
+              <option value="Teknik Informatika">Teknik Informatika</option>
+              <option value="Teknik Mesin">Teknik Mesin</option>
+              <option value="Teknik Industri">Teknik Industri</option> 
+              <option value="Teknik Pangan">Teknik Pangan</option>
+              <option value="Teknik Planologi">Teknik Planologi</option> 
+              <option value="Teknik lingkungan">Teknik lingkungan</option> 
+              <option value="Teknik Memakan Pisang">Teknik Memakan Pisang</option> 
+            </select>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-primary">Tambah Data</button>
+      </form>   
+      </div>
+    </div>
+  </div>
 </div>
